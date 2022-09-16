1529131e8e272137029d846aa6102c5b0d67c2db
Taras-Kutsenko <taras.kutsenko@hotmail.com>
2022-09-16 20:16:02 +0200

    Continue lesson

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,4 +1,4 @@
-let dict : [String : String] = ["car" : "Mercedes", "man" : "Taras"] //Не может быть два одинаковых ключа
+var dict : [String : String] = ["car" : "Mercedes", "man" : "Taras"] //Не может быть два одинаковых ключа
 let dict2 : [Int : String] = [0 : "car", 1 : "Taras"]
 let dict3 : Dictionary<String, Double> = ["a" : 2.0]//Можено еще так записывать
 
@@ -17,3 +17,40 @@ var dict5 = [String : String]()
 dict5.count
 dict5.isEmpty
 
+dict["Computer"] = "Mac"//Так добавлять значение с ключом к Dictionary но так же можно заменить значение в существующем ключе
+print(dict)
+
+dict.keys//Можем взяь с Dictionary ключи
+dict.values//Можем взяь с Dictionary значения
+//Dictionary не распологает значения в том порядке в котором мы их вносили, он распологает их хаотично
+
+dict.updateValue("HP", forKey: "Computer")//Еще один способ как можно поменять значение при этом возвращает пред старое значение а если не найдет такого ключа то вернет nil
+
+//let comp = dict["Computer"]//Dictionary не возвращает типизированое значение, а возвращает опциональное, поэтому нужно делать ForsUnRap
+
+if let comp = dict["Computer"] {
+    print("\(comp)")
+} else {
+    print("no value for key Computer")//Можно так UnRap-нуть
+}
+
+//Чтоб убрать значение нужно сделать следущее
+dict["man"] = nil
+print("\(dict)\n")
+//Или использовать метод
+dict.removeValue(forKey: "car")
+dict
+//Чтоб обнулить или метод removeAll или:
+dict = [:]
+dict.isEmpty
+//Как мы можем вывести значения?...
+
+for i in dict4.keys {
+    print("key = \(i), value = \(dict4[i]!)")
+}
+print("")
+//or
+for (key, value) in dict4 {
+    print("key = \(key), value = \(value)")//В этом случае сразу UnRap-нется
+}
+print("")
